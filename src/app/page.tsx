f42ec6106bec11c0b4c8872ab1f2aaf2a95be3c6
John-Kingsley <117424081+thatkhay@users.noreply.github.com>
2025-08-13 12:49:15 +0100

    background push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen font-bold">
+    <div className="flex items-center justify-center h-screen font-bold text-black">
       tech nest, pls work on nav bar. dont bother about the wrong things.
     </div>
   );
